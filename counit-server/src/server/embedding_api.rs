06409d868dc40d03240e6853ddd37ccd66fd885f
Phodal Huang <h@phodal.com>
2023-08-11 22:04:50 +0800

    feat: add operation request

--- counit-server/src/server/embedding_api.rs
+++ counit-server/src/server/embedding_api.rs
@@ -21,3 +21,33 @@ pub struct ResetApiRequest {
 pub struct RestApi {
     id: u64,
 }
+
+#[derive(Debug)]
+pub struct RestApiOperation {
+    // default: 200
+    code: i32,
+    http_method: String,
+    // http uri path
+    path: String,
+    protocols: String,
+    // aka, description
+    description: String,
+
+    request: Option<String>,
+    request_headers: Vec<String>,
+    // should be canonicalName = packageName + "." + className, canonicalName
+    response: Option<String>,
+    response_headers: Vec<String>,
+    // "List", "Set" or "Map"
+    // response_container: Option<String>,
+    // // response type: application/json, application/xml
+    // produces: Vec<String>,
+    // // request type: application/json, application/xml
+    // consumes: String,
+
+    nickname: Option<String>,
+    notes: Option<String>,
+    tags: Vec<String>,
+    authorizations: Vec<String>,
+    extensions: Vec<String>
+}
